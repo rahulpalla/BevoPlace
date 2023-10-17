15b795e7d69360954a8c533cfa28955eae31634b
navitadhillon <73406046+navitadhillon@users.noreply.github.com>
2023-10-17 10:17:38 -0500

    profile and settings pages

---
+++ BevoPlace/ProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.swift
+//  BevoPlace
+//
+//  Created by Navita Dhillon on 10/17/23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var displayNameTextField: UITextField!
+    @IBOutlet weak var displayEmailTextField: UITextField!
+    @IBOutlet weak var otherInfoTextField: UITextField!
+    @IBOutlet weak var saveChangesButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSaveChangesButtonPressed(_ sender: Any) {
+        // add functionality to change info in firebase
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BevoPlace/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  BevoPlace
+//
+//  Created by Navita Dhillon on 10/17/23.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var lightModeLabel: UILabel!
+    @IBOutlet weak var soundModeLabel: UILabel!
+    @IBOutlet weak var lightModeSwitch: UISwitch!
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    @IBOutlet weak var saveChangesButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lightModeLabel.text = "Light Mode Activated:"
+        soundModeLabel.text = "Sound Off:"
+    }
+    
+    @IBAction func onLightModeSwitched(_ sender: Any) {
+        lightModeLabel.text = "Dark Mode Activated:"
+        // add in functionality for dark mode
+    }
+    
+    @IBAction func onSoundModeSwitched(_ sender: Any) {
+        soundModeLabel.text = "Sound On:"
+        // add in functionality for sound on
+    }
+    
+    @IBAction func onSaveChangesButtonPressed(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
