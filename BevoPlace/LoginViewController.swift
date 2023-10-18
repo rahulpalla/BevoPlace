2a60b62db27a56e18340be0a136aca35097593e2
Rahul Palla <rahulpalla@utexas.edu>
2023-10-17 22:19:14 -0500

    added firebase auth

--- BevoPlace/LoginViewController.swift
+++ BevoPlace/LoginViewController.swift
@@ -10,7 +10,7 @@ import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var emailTextField: UITextField!
+        @IBOutlet weak var emailTextField: UITextField!
         
         @IBOutlet weak var passwordTextField: UITextField!
         
@@ -28,10 +28,11 @@ class LoginViewController: UIViewController {
             super.viewDidLoad()
             super.viewDidLoad()
             passwordTextField.isSecureTextEntry = true
+            
             Auth.auth().addStateDidChangeListener() {
                 (auth,user) in
                 if user != nil {
-                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                    self.performSegue(withIdentifier: "loginToTabSegue", sender: nil)
                     self.emailTextField.text = nil
                     self.passwordTextField.text = nil
                 }
@@ -43,25 +44,31 @@ class LoginViewController: UIViewController {
         }
         
         @IBAction func loginButtonPressed(_ sender: Any) {
-            if emailTextField.text == "" {
-                self.errorLabel.text = "Please enter your username"
-            } else if passwordTextField.text == "" {
-                self.errorLabel.text = "Please enter your password"
-            } else {
-                Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { authResult, error in
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {
+                (authResult,error) in
                     if let error = error as NSError? {
                         self.errorLabel.text = "\(error.localizedDescription)"
                     } else {
-                        self.errorLabel.text = "Success!"
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            self.performSegue(withIdentifier: "loginToHomeSegue", sender: self)
-                        }
+                        self.errorLabel.text = ""
                     }
-                }
             }
+//            if emailTextField.text == "" || emailTextField.text == nil {
+//                self.errorLabel.text = "Please enter your username"
+//            } else if passwordTextField.text == "" || passwordTextField.text == nil {
+//                self.errorLabel.text = "Please enter your password"
+//            } else {
+//                    Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { authResult, error in
+//                        if let error = error as NSError? {
+//                            self.errorLabel.text = "\(error.localizedDescription)"
+//                        } else {
+//                            self.errorLabel.text = "Success!"
+//                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                                self.performSegue(withIdentifier: "loginToTabSegue", sender: self)
+//                            }
+//                        }
+//                    }
+//            }
         }
-
-
-
+    
 }
 
--- BevoPlace/SignUpViewController.swift
+++ BevoPlace/SignUpViewController.swift
@@ -28,33 +28,32 @@ class SignUpViewController: UIViewController {
         
         override func viewDidLoad() {
             super.viewDidLoad()
+            signUpPasswordTextField.isSecureTextEntry = true
+            signUpConfirmPasswordTextField.isSecureTextEntry = true
         }
         
         @IBAction func signupButtonPressed(_ sender: Any) {
             //custom error messages for empty fields
-            if signUpEmailTextField.text == "" {
-                self.signUpStatusLabel.text = "Please enter a username"
-            } else if signUpPasswordTextField.text == "" {
-                self.signUpStatusLabel.text = "Please enter a password"
-            } else if signUpConfirmPasswordTextField.text == "" {
-                self.signUpStatusLabel.text = "Please confirm password"
-            } else if signUpPasswordTextField.text != signUpConfirmPasswordTextField.text {
-                self.signUpStatusLabel.text = "Passwords do not match"
-            } else {
-                // if sign up error, display error
-                // otherwise, show success message and move to main view controller (i.e. pizza table view)
+//            if signUpEmailTextField.text == "" {
+//                self.signUpStatusLabel.text = "Please enter a username"
+//            } else if signUpPasswordTextField.text == "" {
+//                self.signUpStatusLabel.text = "Please enter a password"
+//            } else if signUpConfirmPasswordTextField.text == "" {
+//                self.signUpStatusLabel.text = "Please confirm password"
+//            } else if signUpPasswordTextField.text != signUpConfirmPasswordTextField.text {
+//                self.signUpStatusLabel.text = "Passwords do not match"
+//            } else {
                 Auth.auth().createUser(withEmail: signUpEmailTextField.text!, password: signUpPasswordTextField.text!) { authResult, error in
                     if let error = error as NSError? {
                         self.signUpStatusLabel.text = "\(error.localizedDescription)"
                     } else {
-                        // extra: shows successful login message and delays for 0.5 seconds so message can be seen
                         self.signUpStatusLabel.text = "Success!"
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            self.performSegue(withIdentifier: "signUpToHomeSegue", sender: self)
+                            self.performSegue(withIdentifier: "signUpToTabSegue", sender: self)
                         }
                     }
                 }
-            }
+            //}
         
         }
 
