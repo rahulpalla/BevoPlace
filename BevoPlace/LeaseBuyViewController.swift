f1697c71c73d03cc342bea387e2e5db601fccd90
Shaz Momin <shaz.momin@utexas.edu>
2023-11-03 17:38:20 -0500

    message

--- BevoPlace/LeaseBuyViewController.swift
+++ BevoPlace/LeaseBuyViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 import FirebaseAuth
 
 public var items = [
-    Product(id: 1, name: "Vintage Texas Sweatshirt", description: "Lightly worn sweatshirt, burnt orange gameday fit", userID: 1, image: "https://www.google.com", lease: true, price: 5, numPeriods: 3, size: Size.L)
+    Product(id: 1, name: "Vintage Texas Sweatshirt", description: "Lightly worn sweatshirt, burnt orange gameday fit", userID: 1, image: "https://www.google.com", lease: true, price: 25,
+            period: Period.week ,numPeriods: 3, size: Size.L)
 ]
 
 class LeaseBuyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -33,11 +34,21 @@ class LeaseBuyViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = itemTableView.dequeueReusableCell(withIdentifier: itemCellIdentifier, for: indexPath as IndexPath)
+        let cell = itemTableView.dequeueReusableCell(withIdentifier: itemCellIdentifier, for: indexPath as IndexPath) as! ProductCell
 
         let row = indexPath.row
-        cell.textLabel?.text = items[row].name
-        cell.detailTextLabel?.text = items[row].description
+        cell.productTitleLabel.text = items[row].name
+        cell.productSizeLabel.text = "Size: \(String(describing: items[row].size))"
+        if (!items[row].lease) {
+            // Buy Item interface
+            cell.productPriceLabel.text = "Price: $\(String(round(items[row].price)))"
+            cell.leaseLengthLabel.text = ""
+        } else {
+            // Lease Item interface
+            cell.productPriceLabel.text = "Price: $\(String(round(items[row].price)))/\(String(describing: items[row].period))"
+            cell.leaseLengthLabel.text = "Lease length: \(items[row].numPeriods) \(String(describing: items[row].period))s"
+        }
+        
         return cell
     }
 
--- BevoPlace/ProductCell.swift
+++ BevoPlace/ProductCell.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+class ProductCell: UITableViewCell {
+    @IBOutlet weak var ProductImage: UIImageView!
+    @IBOutlet weak var productTitleLabel: UILabel!
+    @IBOutlet weak var productSizeLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var ctaButton: UIButton!
+    @IBOutlet weak var leaseLengthLabel: UILabel!
+    
+    @IBAction func ctaButtonClicked(_ sender: Any) {
+    }
+}
