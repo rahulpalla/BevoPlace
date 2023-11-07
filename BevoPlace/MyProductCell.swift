aafd83da8c6fd8116367a9814b7d8a783e40319b
navitadhillon <73406046+navitadhillon@users.noreply.github.com>
2023-11-06 19:35:42 -0600

    kinda works lend/sell vc feed

---
+++ BevoPlace/MyProductCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyProductCell.swift
+//  BevoPlace
+//
+//  Created by Navita Dhillon on 11/6/23.
+//
+
+import Foundation
+import UIKit
+
+class MyProductCell: UITableViewCell {
+    
+    @IBOutlet weak var productTitleLabel: UILabel!
+    
+    @IBOutlet weak var productSizeLabel: UILabel!
+    
+    @IBOutlet weak var leaseLengthLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+}
